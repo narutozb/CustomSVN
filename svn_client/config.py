d984bf51ea6412c543005fdc5802927df859bc91
qiaoyuanzhen <narutozb@hotmail.com>
2024-05-28 00:23:36 +0900

    修改配置文件

--- svn_client/config.py
+++ svn_client/config.py
@@ -3,6 +3,8 @@
 class Config:
     ROOT_URL = 'http://127.0.0.1:8000'
     API_URL = f'{ROOT_URL}/api/'
+    REPO_URL = 'https://qiaoyuanzhen/svn/TestRepo/'
+    REPO_NAME = 'TestRepo'
     USERNAME = 'admin'
     PASSWORD = 'adminadmin'
     MAX_UPLOAD_SIZE = 200 * 1024 * 1024  # 200 MB in bytes
