4dff17d3415f741c68aac9c215fc47662802ca48
qiaoyuanzhen <narutozb@hotmail.com>
2024-06-15 16:34:48 +0900

    更改默认配置

--- svn_client/config.py
+++ svn_client/config.py
@@ -1,7 +1,7 @@
 # config.py
 
 class Config:
-    COMMITS_SPLIT_NUM = 50
+    COMMITS_SPLIT_NUM = 10
     ROOT_URL = 'http://127.0.0.1:8000'  # 'http://127.0.0.1:8000'
 
     REPO_URL = 'https://qiaoyuanzhen/svn/MyDataSVN/'  # 自定义服务器端的url标识
@@ -11,8 +11,8 @@ class Config:
     USERNAME = 'admin'
     PASSWORD = 'adminadmin'
     MAX_UPLOAD_SIZE = 200 * 1024 * 1024  # 200 MB in bytes
-    START_REVISION = 1  # 默认起始 revision
-    END_REVISION = 20  # 默认终止 revision
+    START_REVISION = None  # 默认起始 revision
+    END_REVISION = None  # 默认终止 revision
     MAYA_DATA_API_URL = f'{ROOT_URL}/'
     SVN_UPDATE_INTERVAL = 15  # svn检查和更新数据的间隔。秒
     RUN_ONCE = True  # 新增选项，控制程序是否只运行一次
