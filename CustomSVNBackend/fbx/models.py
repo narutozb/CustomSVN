307de23e753eb8afdfb1e4d5c35e33f8db939098
qiaoyuanzhen <narutozb@hotmail.com>
2024-06-17 00:49:41 +0900

    制作储存ModelSkeleton的视图

--- CustomSVNBackend/fbx/models.py
+++ CustomSVNBackend/fbx/models.py
@@ -1,3 +1,11 @@
+from django.db import models
+
+from svn.models import FileChange
+
+
+# Create your models here.
+
+
 class FBXFile(models.Model):
     file_change = models.OneToOneField(FileChange, on_delete=models.CASCADE, null=True, blank=True)
     fps = models.FloatField(blank=True, null=True)
@@ -66,64 +74,3 @@ class TakeModelSkeleton(models.Model):
 
     class Meta:
         unique_together = ('take', 'model_skeleton',)
-=======
-class Fbx(models.Model):
-    changed_file = models.OneToOneField(FileChange, on_delete=models.CASCADE, related_name='fbx_file')
-    opened_successfully = models.BooleanField(null=True, blank=True)
-    description = models.TextField(blank=True, null=True)
-    local_path = models.CharField(max_length=255, null=True, blank=True)
-
-    class Meta:
-        verbose_name = 'FBX'
-        verbose_name_plural = verbose_name
-
-    def __str__(self):
-        return f'FBX for {self.changed_file.file_path}'
-
-
-class FbxDetail(models.Model):
-    fbx = models.OneToOneField(Fbx, on_delete=models.CASCADE, related_name='details')
-    fps = models.FloatField()
-
-
-class Character(models.Model):
-    name = models.CharField(max_length=255)
-    fbx_detail = models.ForeignKey(FbxDetail, on_delete=models.CASCADE, related_name='characters')
-
-
-class FbxTake(models.Model):
-    fbx_detail = models.ForeignKey(FbxDetail, on_delete=models.CASCADE, related_name='takes')
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f'FBX Take {self.name}'
-
-
-class FbxAnimLayer(models.Model):
-    '''
-    Take 001: BaseAnimation
-    Take 001: AnimLayer1
-
-    '''
-    fbx_detail = models.ForeignKey(FbxTake, on_delete=models.CASCADE, related_name='layers')
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f'FBX AnimLayer {self.name}'
-
-
-class NodeAttribute(models.Model):
-    node_name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
-
-    def clean(self):
-        if self.parent and self.parent == self:
-            raise ValidationError("A node cannot be its own parent.")
-
-    class Meta:
-        abstract = True
-
-
-class Node(NodeAttribute):
-    fbx = models.ForeignKey(Fbx, on_delete=models.CASCADE, related_name='nodes')
-
