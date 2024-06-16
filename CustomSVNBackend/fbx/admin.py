471bc3eac21019837f8323e7d6fdf6e2a85fce0a
qiaoyuanzhen <narutozb@hotmail.com>
2024-06-16 16:34:04 +0900

    增加fbx应用模型类

---
+++ CustomSVNBackend/fbx/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import FBXFile, Take, ModelSkeleton, TakeModelSkeleton
+
+
+@admin.register(FBXFile)
+class FBXFileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fps', 'file_change',)
+
+
+@admin.register(Take)
+class TakeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fbx_file', 'name', 'start_frame', 'end_frame',)
+
+
+@admin.register(ModelSkeleton)
+class ModelSkeletonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'parent',)
+
+
+@admin.register(TakeModelSkeleton)
+class ModelSkeletonKeyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'take', 'model_skeleton', 'ws_location', 'ws_rotation', 'ws_scale', 'frame',)
+
+
+
+
+
