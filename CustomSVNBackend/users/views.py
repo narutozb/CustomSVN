ad89a307fdd66404816e4ff6f0ed4aa30a9a9747
qiaoyuanzhen <narutozb@hotmail.com>
2024-09-16 20:31:35 +0900

    整理代码

--- CustomSVNBackend/users/views.py
+++ CustomSVNBackend/users/views.py
@@ -14,19 +14,6 @@ from .models import CustomUser
 from .serializers import UserSerializer, CustomTokenObtainPairSerializer, CustomTokenRefreshSerializer
 
 
-class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email
-        })
-
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
